c82eb05003771cacc8c23d44eb7731b06f0c84ad
Elizabeth Sham <esham00@stuy.edu>
2019-03-15 11:50:02 -0400

    classwork

---
+++ Merge.java
@@ -0,0 +1,10 @@
+public class Merge {
+    public static void mergesort(int[] data) {
+    }
+    public static void mergesortH(int[] data, int lo, int hi) {
+	if (lo >= hi) {
+	    return;
+	} else {
+	    mergesortH();
+	    mergesortH();
+	    data[
