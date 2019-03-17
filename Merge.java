66fa2241c49fb527937456eb90c26791ba213f9a
Elizabeth Sham <esham00@stuy.edu>
2019-03-17 18:57:33 -0400

    merge is not being recursively called in the second half

--- Merge.java
+++ Merge.java
@@ -3,24 +3,44 @@ public class Merge {
     public static void mergesort(int[] data) {
 	mergesortH(data, 0, data.length-1);
     }
-    public static void merge(int[] data, int hi){
-	int[] left = new int[hi/2];
-	int[] right = new int[hi/2];
+    public static String toString(int[] data, int low, int hi) {
+	String output = "";
+	for(int i = low; i < hi; i++) {
+	    output += data[i];
+	}
+	return output;
+    }
+    public static void merge(int[] data, int low, int hi){
+	int[] left = new int[(hi-low)/2];
+	int[] right = new int[(hi-low)/2];
 	for(int i = 0; i < left.length; i++) {
 	    left[i] = data[i];
 	    right[i] = data[i+left.length];
 	}
+	System.out.println(Arrays.toString(left));
+	System.out.println(Arrays.toString(right));
+	
 	int i = 0;
 	int j = 0;
-	int dataIndex = 0;
+	int dataIndex = low;
 	while(i < left.length && j < right.length) {
-	    if (left[i] < right[j]) {
+	    if (left[i] <= right[j]) {
 		data[dataIndex] = left[i];
 		i++;
 	    } else {
 		data[dataIndex] = right[j];
 		j++;
 	    }
+	    dataIndex++;
+	}
+	while (i < left.length) {
+	    data[dataIndex] = left[i];
+	    i++;
+	    dataIndex++;
+	}
+	while (j < right.length) {
+	    data[dataIndex] = right[j];
+	    j++;
 	    dataIndex++;
 	}
     }
@@ -30,7 +50,7 @@ public class Merge {
 	} else {
 	    mergesortH(data, lo, hi/2);
 	    mergesortH(data, (hi-lo)/2 + (data.length/2), hi);
-	    merge(data, hi);
+	    merge(data, lo, hi);
 	}
     }
     public static void main(String[] args) {
